2eac07962ea07efc9acd61856a6e7d2a76fc6ca9
Moniquilina <mmich2006@gmail.com>
2025-03-16 22:10:43 +0100

    Fonction 1 Monique

--- testdacine.py
+++ testdacine.py
@@ -2,13 +2,18 @@
 import random
 
 def cree_fichier_alea(nb, nomfichier):
-    with open("fichiertest.txt", "w") as fichier:
+    fichier= nomfichier
+    with open(nomfichier, "w") as fichier:
         nb1= random.random()
         nb2= random.random()
-        for i in range (nb):
-            fichier.write(nb1)
+        for i in range (nb):            
+            fichier.write(str(nb1))
+            fichier.write(str(nb2)  + "\n")
+    fichier.readlines()
     print(fichier)
-cree_fichier_alea(1, "n")
+
+cree_fichier_alea(2, "Ok.txt")
+
 
     
         
