1aff38e7653aa350f2f6a2f4979daae6b5b53b14
Moniquilina <mmich2006@gmail.com>
2025-03-16 19:00:45 +0100

    1ère modif Monique

--- testdacine.py
+++ testdacine.py
@@ -1,1 +1,19 @@
-print("test")
+
+import random
+
+def cree_fichier_alea(nb, nomfichier):
+    with open("fichiertest.txt", "w") as fichier:
+        nb1= random.random()
+        nb2= random.random()
+        for i in range (nb):
+            fichier.write(nb1)
+    print(fichier)
+cree_fichier_alea(1, "n")
+
+    
+        
+    
+  
+
+    
+
