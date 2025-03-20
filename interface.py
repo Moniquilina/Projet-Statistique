8d7a94d496555a7d537225b0bd0b87fc662af586
dacine06 <ammaridacine@gmail.com>
2025-03-20 18:39:16 +0100

    creation du canevas

--- interface.py
+++ interface.py
@@ -1,4 +1,6 @@
 import tkinter as tk 
 import random
-def tracer_droite()
-    
+fenetre = Tk()
+fenetre.title("interface V1")
+cane = Canvas(fenetre, width=800, height=800, bg='white')
+cane.grid(row=0, column=0, columnspan=3, pady=0)
