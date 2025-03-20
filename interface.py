d65709af64e406cf238ce9b40f8a6a12f7ed5cdc
dacine06 <ammaridacine@gmail.com>
2025-03-20 19:10:28 +0100

    quelques annotation et aussi le commit precedent avait la creation de bouton j ai oublier de le preciser

--- interface.py
+++ interface.py
@@ -2,16 +2,19 @@ import tkinter as tk
 import random
 fenetre = tk.Tk()
 fenetre.title("interface V1")
+#je creer le canvas
 canevas = tk.Canvas(fenetre, width=800, height=800, bg='white')
 canevas.grid(row=0, column=0, columnspan=3, pady=0)
-couleurs = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
+couleurs = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'] #liste des couleurs dispo
 couleur_actuelle = 'red'
 def tracer_ligne():
+    #juste pour tester 
     canevas.create_line(10, 10, 200, 800, fill=couleur_actuelle, width=3)
 
-def changer_couleur():
+def changer_couleur():#definir la fonction pour le changement de couleur 
     global couleur_actuelle
     couleur_actuelle = random.choice(couleurs)
+#creer les boutons 
 
 bouton_tracer = tk.Button(fenetre, text="tracer la droite", command=tracer_ligne)
 bouton_tracer.grid(row=1, column=0)
