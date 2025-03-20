07a9925e8463482b25c850abe2291aba4e2778be
dacine06 <ammaridacine@gmail.com>
2025-03-20 19:04:28 +0100

    interface essay

--- interface.py
+++ interface.py
@@ -1,6 +1,25 @@
 import tkinter as tk 
 import random
-fenetre = Tk()
+fenetre = tk.Tk()
 fenetre.title("interface V1")
-cane = Canvas(fenetre, width=800, height=800, bg='white')
-cane.grid(row=0, column=0, columnspan=3, pady=0)
+canevas = tk.Canvas(fenetre, width=800, height=800, bg='white')
+canevas.grid(row=0, column=0, columnspan=3, pady=0)
+couleurs = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
+couleur_actuelle = 'red'
+def tracer_ligne():
+    canevas.create_line(10, 10, 200, 800, fill=couleur_actuelle, width=3)
+
+def changer_couleur():
+    global couleur_actuelle
+    couleur_actuelle = random.choice(couleurs)
+
+bouton_tracer = tk.Button(fenetre, text="tracer la droite", command=tracer_ligne)
+bouton_tracer.grid(row=1, column=0)
+
+bouton_couleur = tk.Button(fenetre, text="Autre couleur", command=changer_couleur)
+bouton_couleur.grid(row=1, column=1)
+
+bouton_quitter = tk.Button(fenetre, text="Quitter", command=fenetre.quit)
+bouton_quitter.grid(row=1, column=2)
+
+fenetre.mainloop()
