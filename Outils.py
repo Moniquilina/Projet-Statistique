3c61b3a938a4aaede3f6592cee4897bf775dff8d
Moniquilina <mmich2006@gmail.com>
2025-03-21 11:50:58 +0100

    Modifs

--- Outils.py
+++ Outils.py
@@ -5,17 +5,34 @@ def cree_fichier_alea(nb, nomfichier):
     fichier= nomfichier
     with open(nomfichier, "w") as fichier:
         for i in range (nb):
-            nb1= random.random()
+            nb1= random.random() #nb1 et nb2 =abscisses et ordonées
             nb2= random.random()
-            fichier.write(str(nb1)+"")
+            fichier.write(str(nb1)+"") #Fonction qui écrit les nombres des abscisses et ordonées dans le fichier généré
             fichier.write(str(nb2)+ "\n")
     fichier.readlines()
     print(fichier)
 
-cree_fichier_alea(2, "fichiertest.txt")
+cree_fichier_alea(5, "fichiertest.txt")
 
 def lit_fichier(nomfic):
-    fichier2=nomfic
-    with open(nomfic, "w") as fichier:
+    fichier_nombres= cree_fichier_alea
+    fichier_nombres.read()
+    fichier_listes=nomfic
+    with open(nomfic, "w") as fichier_listes:
+        LX= []
+        LY = []
+        for i in range (fichier_nombres):
+            while ligne:
+                print(ligne.strip())
+                ligne = fichier_listes.readline()
+                
+    print(fichier_listes)
+lit_fichier("xxx.txt")
+        
+
+
+
+   
+
 
 
