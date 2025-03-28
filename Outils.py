b6c780ef6750975d0ae822784b5f4d0da7e0f8e6
Moniquilina <mmich2006@gmail.com>
2025-03-28 12:35:24 +0100

    Fichiers

--- Outils.py
+++ Outils.py
@@ -2,12 +2,12 @@
 import random
 
 def cree_fichier_alea(nb, nomfichier):
-    fichier= nomfichier
+    fichier = nomfichier    
     with open(nomfichier, "w") as fichier:
         for i in range (nb):
             nb1= random.random() #nb1 et nb2 =abscisses et ordonées
             nb2= random.random()
-            fichier.write(str(nb1)+"") #Fonction qui écrit les nombres des abscisses et ordonées dans le fichier généré
+            fichier.write(str(nb1)+ "") #Fonction qui écrit les nombres des abscisses et ordonées dans le fichier généré
             fichier.write(str(nb2)+ "\n")
     fichier.readlines()
     print(fichier)
@@ -15,24 +15,22 @@ def cree_fichier_alea(nb, nomfichier):
 cree_fichier_alea(5, "fichiertest.txt")
 
 def lit_fichier(nomfic):
-    fichier_nombres= cree_fichier_alea
-    fichier_nombres.read()
-    fichier_listes=nomfic
-    with open(nomfic, "w") as fichier_listes:
-        LX= []
-        LY = []
-        for i in range (fichier_nombres):
-            while ligne:
-                print(ligne.strip())
-                ligne = fichier_listes.readline()
-                
-    print(fichier_listes)
-lit_fichier("xxx.txt")
-        
+    fichier = nomfic
+    LX= []
+    LY = [] 
+    with open("files/fichier.txt", "r") as fichier: 
+        fichier.readlines()    #cela va permettre d'ouvrir le fichier qui contien les coordonnées et de stocker les valeurs
+    with open(nomfic, "w") as fichier_c:
+        while fichier == int:
+            LX.append(int(fichier))
+            fichier_c.write(LX)
+    print(fichier_c)
 
+#def trace_Nuage(nomf):
 
+    
+    
+    
+cree_fichier_alea(5, "fichiertest.txt")
 
-   
-
-
-
+#lit_fichier("coordonnées.txt")
