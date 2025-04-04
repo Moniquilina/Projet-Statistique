4fb8408c1071f34a27ac3ff8ba2eaecf842cfaba
Moniquilina <mmich2006@gmail.com>
2025-04-04 12:05:40 +0200

    Listes

--- Outils.py
+++ Outils.py
@@ -7,9 +7,10 @@ def cree_fichier_alea(nb, nomfichier):
         for i in range(nb):       
             nb1= random.random() #nb1 et nb2 =abscisses et ordonées
             nb2= random.random()
-            fichier.write(str(nb1) +str(";")) #Fonction qui écrit les nombres des abscisses et ordonées dans le fichier généré
-            fichier.write(str(nb2)+ "\n")
-        
+            fichier.write(str(nb1) + ";") 
+            fichier.write(str(nb2) + "\n") #Fonction qui écrit les nombres des abscisses et ordonées dans le fichier généré
+            
+            
     
     print(fichier)
 
@@ -17,17 +18,18 @@ def lit_fichier(nomfic):
     fichier = nomfic
     LX= []
     LY =[] 
-    with open("fichiertest.txt", "r") as fichier: 
-        C =fichier.readlines()        #cela va permettre d'ouvrir le fichier qui contient les coordonnées et de stocker les valeurs
-        
-        for i in C :
-            
-            LX.append(C)
-            #LY.append(C)
+    with open("fichiertest.txt", "r") as fichier:
 
-    print(LX)
+        C =fichier.read().split(";")      #cela va permettre d'ouvrir le fichier qui contient les coordonnées et de stocker les valeurs dans une liste
+        for i in C :
+                        
+            LX.append(C[0])
+            LY.append(C[1])
+
+    print(LX, LY)
     
     with open(nomfic, "w") as fichier:
+        
         fichier.write(str(LX))
         fichier.write(str(LY))
     print(fichier)
