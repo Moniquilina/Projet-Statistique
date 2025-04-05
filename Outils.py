64ff7af19e2012ec240e0a9bd4a6bdc89ce408ac
Moniquilina <mmich2006@gmail.com>
2025-04-05 19:07:51 +0200

    Listes

--- Outils.py
+++ Outils.py
@@ -7,7 +7,7 @@ def cree_fichier_alea(nb, nomfichier):
         for i in range(nb):       
             nb1= random.random() #nb1 et nb2 =abscisses et ordonées
             nb2= random.random()
-            fichier.write(str(nb1) + ";") 
+            fichier.write(str(nb1) + "\t") 
             fichier.write(str(nb2) + "\n") #Fonction qui écrit les nombres des abscisses et ordonées dans le fichier généré
             
             
@@ -17,27 +17,29 @@ def cree_fichier_alea(nb, nomfichier):
 def lit_fichier(nomfic):
     fichier = nomfic
     LX= []
-    LY =[] 
+    LY =[]
     with open("fichiertest.txt", "r") as fichier:
-
-        C =fichier.read().split(";")      #cela va permettre d'ouvrir le fichier qui contient les coordonnées et de stocker les valeurs dans une liste
-        for i in C :
-                        
-            LX.append(C[0])
-            LY.append(C[1])
+        Cor = fichier.read().split("\t")
+        #C =Cor.split(", ")      #cela va permettre d'ouvrir le fichier qui contient les coordonnées et de stocker les valeurs dans une liste
+        print(Cor)
+        for i in Cor :            
+                            
+            LX.append(Cor)
+            #LY.append(C[1])
 
     print(LX, LY)
     
     with open(nomfic, "w") as fichier:
         
         fichier.write(str(LX))
-        fichier.write(str(LY))
+        #fichier.write(str(LY))
     print(fichier)
             
     
     
 
-#def trace_Nuage(nomf):
+def trace_Nuage(nomf):
+    with open("coordonnées.txt", "r") as fichier:
 
     
     
