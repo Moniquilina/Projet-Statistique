776fdca19abac34716d32d32edee92b38ebe61c8
Moniquilina <mmich2006@gmail.com>
2025-05-03 00:36:46 +0200

    test des fichiers

--- Outils.py
+++ Outils.py
@@ -3,8 +3,10 @@ import random
 import re
 import tkinter as tk
 
-
-
+CANVAS_WIDTH, CANVAS_HEIGHT = 800, 600
+l=40
+root= tk.Tk()
+canva = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
 def cree_fichier_alea(nb, nomfichier):
     fichier = nomfichier    
     with open(nomfichier, "w") as fichier: 
@@ -50,7 +52,7 @@ def trace_Nuage(nomf):
         LY= Cor[1]
 #
         LX=re.findall(r'[-+]?[0-9]*\.?[0-9]+', LX)
-        LY=re.findall(r'[-+]?[0-9]*\.?[0-9]+', LY)              #re.findall permet d'extraire les nombres de la liste qui était sous forme de chaîne de caractères
+        LY=re.findall(r'[-+]?[0-9]*\.?[0-9]+', LY)              #re.findall permet d'extraire les nombres de la liste qui étaient sous forme de chaîne de caractères
 
 
         i=0
@@ -69,69 +71,60 @@ def trace_Nuage(nomf):
     my= min(LY)
     may = max(LY)
 
-        
-
-    CANVAS_WIDTH, CANVAS_HEIGHT = 800, 600
-    x=50
-    root = tk.Tk()        
-    canva = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
-    
-    canva.create_line(x, CANVAS_HEIGHT - x, CANVAS_WIDTH - x, CANVAS_HEIGHT - x) 
-    canva.create_line(x, x, x, CANVAS_HEIGHT - x)  
+    canva.create_line(l, CANVAS_HEIGHT - l, CANVAS_WIDTH - l, CANVAS_HEIGHT - l) 
+    canva.create_line(l, l, l, CANVAS_HEIGHT - l)  
           
-    for x1,y1 in zip(LX,LY): 
-                                     
-        px= x+ (x1- m) / (ma - m) * (CANVAS_WIDTH-2*x)
-        py = CANVAS_HEIGHT-(x+ (y1 - my) / (may - my) *(CANVAS_HEIGHT-2*x))
-       
+    for x1,y1 in zip(LX,LY):                                                    #conversion des coordonnées dans des pixels
+                                             
+        px= l+ (x1- m) / (ma - m) * (CANVAS_WIDTH-2*l)
+        py = CANVAS_HEIGHT-(l+ (y1 - my) / (may - my) *(CANVAS_HEIGHT-2*l))
         
-        canva.create_oval(px-3, py-3, px+3, py+3)
-             
-
-       
-    canva.pack()
-    root.mainloop()
+        
+        canva.create_oval(px-3, py-3, px+3, py+3, fill ="green", outline="green")
+        
             
    
     with open(nomf, "w") as fichier:
         fichier.write("Nombre de points:")
         fichier.write(str(len(LX)))
+            
         
     print(fichier)
 
-def trace_droite(a, b):
+    
+def trace_droite():
+    
     with open("coordonnées.txt", "r") as fichier:
         X=fichier.readlines()
-        X= X[0]
-        print(X)
-        X=re.findall(r'[-+]?[0-9]*\.?[0-9]+', X)
-
-        X=X[1]
-        X= float(X)
-        print(X)
-
-        x=0    
-        y= a*X + b
-    
+        x=X[0]
+        x=re.findall(r'[-+]?[0-9]*\.?[0-9]+', x)
         
-    
+        print(x)
+        i=random.randint(0,len(x))
+        for j in range(len(x)):
+            x1=x[i]
+        a=20
+        b=1
+        x1=float(x1)
+        y= a*x1+b
+                      
+        canva = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+        px= l+ x1/l* (CANVAS_WIDTH-2*l)
+        py = CANVAS_HEIGHT+(l+ y/l *(CANVAS_HEIGHT-2*l))
+        print(px, py)
+        canva.create_line(px, py, px + CANVAS_WIDTH, py-CANVAS_HEIGHT, fill="black")
 
 
-
-
-
-
-
-
-
-
-
-    
-    
-    
-
-
-cree_fichier_alea(20, "fichiertest.txt")
+cree_fichier_alea(100, "fichiertest.txt")
 lit_fichier("coordonnées.txt")
-trace_Nuage("graph")
-#trace_droite(10,60)
+trace_Nuage("points")
+trace_droite()
+
+root= tk.Tk()
+bouton_droite= tk.Button(root, text ="Tracer droite", command= trace_droite)
+bouton_droite.grid(row=3, column=0)
+   
+
+canva.grid()
+root.mainloop() 
+  
