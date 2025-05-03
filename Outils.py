d0247a34047e7fb3ee2aea731371285750f2e66d
Moniquilina <mmich2006@gmail.com>
2025-05-03 12:45:35 +0200

    Final

--- Outils.py
+++ Outils.py
@@ -11,10 +11,10 @@ def cree_fichier_alea(nb, nomfichier):
     fichier = nomfichier    
     with open(nomfichier, "w") as fichier: 
         for i in range(nb):       
-            nb1= random.random() #nb1 et nb2 =abscisses et ordonées
+            nb1= random.random()                                #nb1 et nb2 =abscisses et ordonées
             nb2= random.random()
             fichier.write(str(nb1) + "\t") 
-            fichier.write(str(nb2) + "\n") #Fonction qui écrit les nombres des abscisses et ordonées dans le fichier généré
+            fichier.write(str(nb2) + "\n")                      #Fonction qui écrit les nombres des abscisses et ordonées dans le fichier généré
             
     print(fichier)
 
@@ -24,7 +24,7 @@ def lit_fichier(nomfic):
     
     with open("fichiertest.txt", "r") as fichier:
         Cor = fichier.read()
-        Cor= re.split(r"[\n, \t]", Cor)      #re.split() permet de separer les valeurs en fonctions des espaces ajoutés dans le fichier 
+        Cor= re.split(r"[\n, \t]", Cor)                         #re.split() permet de separer les valeurs en fonctions des espaces ajoutés dans le fichier 
 
         i=0
         
@@ -34,7 +34,7 @@ def lit_fichier(nomfic):
                 
         LX=Cor[0::2]
         LX.pop()
-        LY = Cor[1::2]                      #Creation des deux listes contenant les coordonées
+        LY = Cor[1::2]                                          #Creation des deux listes contenant les coordonées
             
 
     with open(nomfic, "w") as fichier:
@@ -87,44 +87,33 @@ def trace_Nuage(nomf):
     with open(nomf, "w") as fichier:
         fichier.write("Nombre de points:")
         fichier.write(str(len(LX)))
-            
         
     print(fichier)
 
     
-def trace_droite():
-    
+def trace_droite(a,b):
     with open("coordonnées.txt", "r") as fichier:
         X=fichier.readlines()
         x=X[0]
         x=re.findall(r'[-+]?[0-9]*\.?[0-9]+', x)
-        
-        print(x)
+    
         i=random.randint(0,len(x))
         for j in range(len(x)):
             x1=x[i]
-        a=20
-        b=1
         x1=float(x1)
         y= a*x1+b
-                      
-        canva = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
         px= l+ x1/l* (CANVAS_WIDTH-2*l)
-        py = CANVAS_HEIGHT+(l+ y/l *(CANVAS_HEIGHT-2*l))
-        print(px, py)
-        canva.create_line(px, py, px + CANVAS_WIDTH, py-CANVAS_HEIGHT, fill="black")
-
+        py = CANVAS_HEIGHT-(l+ y/l *(CANVAS_HEIGHT-2*l))
+        canva.create_line(px, py, px + CANVAS_WIDTH, py-CANVAS_HEIGHT, fill="black")       
+        
 
 cree_fichier_alea(100, "fichiertest.txt")
 lit_fichier("coordonnées.txt")
-trace_Nuage("points")
-trace_droite()
-
-root= tk.Tk()
-bouton_droite= tk.Button(root, text ="Tracer droite", command= trace_droite)
-bouton_droite.grid(row=3, column=0)
-   
+trace_Nuage("points.txt")
 
 canva.grid()
 root.mainloop() 
+
+trace_droite(10,2)
+
   
