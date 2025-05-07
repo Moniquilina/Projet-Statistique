d4b96f7d3376381e38b8c5a852910a00fd3bdb07
Moniquilina <mmich2006@gmail.com>
2025-05-07 20:47:59 +0200

    nb10 et nb12

--- lea_3e partie.py
+++ lea_3e partie.py
@@ -19,7 +19,12 @@ print(info)
 # Sélectionner certaines colonnes et lignes spécifiques
 info=iden.loc[[0,1],['nom','date_naissance']]
 print(info)
+def extraire_nombres():
+    # Lire le fichier villes_virgule.csv et afficher toutes les données
+    info_villes=pd.read_csv("villes_virgule.csv")
+    print(info_villes)
+    nb_hab = info_villes.loc[info_villes["nb_hab_2010"]<=500, ["nb_hab_2010"] ]
+    nb_hab_12= info_villes.loc[info_villes["nb_hab_2012"]<=500, ["nb_hab_2012"] ]
+    print(nb_hab, nb_hab_12)
 
-# Lire le fichier villes_virgule.csv et afficher toutes les données
-info_villes=pd.read_csv("villes_virgule.csv")
-print(info_villes)
+extraire_nombres()
